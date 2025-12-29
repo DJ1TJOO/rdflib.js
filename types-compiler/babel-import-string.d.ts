c3fe01c1201a75fbbc9b3bbb1a2e5eb1054b4cbe
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-12-29 16:21:01 +0100

    chore: add support for importing .n3, .ttl, .rdf, .jsonld in tests

---
+++ types-compiler/babel-import-string.d.ts
@@ -0,0 +1,20 @@
+declare module '*.n3' {
+  const content: string
+  export default content
+}
+
+declare module '*.ttl' {
+  const content: string
+  export default content
+}
+
+declare module '*.rdf' {
+  const content: string
+  export default content
+}
+
+declare module '*.jsonld' {
+  const content: string
+  export default content
+}
+
